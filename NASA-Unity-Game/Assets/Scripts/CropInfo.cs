ca4cf38b4db3f97dbbcf5f73c74ce5bf813c828e
andierm <adyrm98@gmail.com>
2024-10-06 00:40:58 +0200

    now it's working (+ text for wheat)

--- NASA-Unity-Game/Assets/Scripts/CropInfo.cs
+++ NASA-Unity-Game/Assets/Scripts/CropInfo.cs
@@ -7,15 +7,20 @@ using UnityEngine.EventSystems;
 
 public class CropInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [TextArea(3, 10)]
     public string cropInfo;
+
+    private float time = 0.3f;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        ShowMessage();
+        StopAllCoroutines();
+        StartCoroutine(StartTime());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        StopAllCoroutines();
         CropInfoPanel.OnMouseExit();
     }
 
@@ -23,5 +28,12 @@ public class CropInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         CropInfoPanel.OnMouseHover(cropInfo, Input.mousePosition);
 
+    }
+
+    private IEnumerator StartTime()
+    {
+        yield return new WaitForSeconds(time);
+
+        ShowMessage();
     }
 }
