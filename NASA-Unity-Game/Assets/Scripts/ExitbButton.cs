41193f4909beca5a733d6589b8d74fee156961a9
andierm <adyrm98@gmail.com>
2024-10-06 22:07:25 +0200

    added turn off button

---
+++ NASA-Unity-Game/Assets/Scripts/ExitbButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitbButton : MonoBehaviour
+{
+ public void ExitGame()
+ {
+    Application.Quit();
+ }
+}
